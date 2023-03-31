5a1f09ace01cf990ff06bae788faeb5f87c7efc6
Antonio Gamero <ags1305@gmail.com>
2023-03-31 18:01:37 +0000

    second try

--- src/js/app.js
+++ src/js/app.js
@@ -62,7 +62,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
